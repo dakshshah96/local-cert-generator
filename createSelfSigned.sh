b9200b50165086f58e523069d538376e4b5c85e9
Daksh Shah <dakshshah96@gmail.com>
2018-01-19 11:52:19 +0530

    Create createSelfSigned.sh

---
+++ createSelfSigned.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+openssl req -new -sha256 -nodes -out server.csr -newkey rsa:2048 -keyout server.key -config <( cat server.csr.cnf )
+openssl x509 -req -in server.csr -CA rootCA.pem -CAkey rootCA.key -CAcreateserial -out server.crt -days 500 -sha256 -extfile v3.ext
