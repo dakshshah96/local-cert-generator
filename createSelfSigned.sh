517b3af50bbfb38cb048fa3631843d77db06f66a
Daksh Shah <dakshshah96@gmail.com>
2020-03-06 13:57:44 +0530

    change self signed script cert days flag to 825

--- createSelfSigned.sh
+++ createSelfSigned.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 openssl req -new -sha256 -nodes -out server.csr -newkey rsa:2048 -keyout server.key -config server.csr.cnf
-openssl x509 -req -in server.csr -CA rootCA.pem -CAkey rootCA.key -CAcreateserial -out server.crt -days 500 -sha256 -extfile v3.ext
+openssl x509 -req -in server.csr -CA rootCA.pem -CAkey rootCA.key -CAcreateserial -out server.crt -days 825 -sha256 -extfile v3.ext
