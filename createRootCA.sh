2512fb5d8ceef18594b282c88042cd5c36c75f94
Budi Arsana <gusdecool@gmail.com>
2020-04-09 10:37:53 +0800

    Move the output to certificate folder

--- createRootCA.sh
+++ createRootCA.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 set -e
-
 mkdir "certificate"
 openssl genrsa -des3 -out certificate/rootCA.key 2048
 openssl req -x509 -new -nodes -key certificate/rootCA.key -sha256 -days 825 -out certificate/rootCA.pem
--- createSelfSigned.sh
+++ createSelfSigned.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-openssl req -new -sha256 -nodes -out server.csr -newkey rsa:2048 -keyout server.key -config server.csr.cnf
-openssl x509 -req -in server.csr -CA rootCA.pem -CAkey rootCA.key -CAcreateserial -out server.crt -days 825 -sha256 -extfile v3.ext
+set -e
+openssl req -new -sha256 -nodes -out certificate/server.csr -newkey rsa:2048 -keyout certificate/server.key -config server.csr.cnf
+openssl x509 -req -in certificate/server.csr -CA certificate/rootCA.pem -CAkey certificate/rootCA.key -CAcreateserial -out certificate/server.crt -days 825 -sha256 -extfile v3.ext
