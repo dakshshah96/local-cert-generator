755ecc9f6f203ae930fdb54fe4b0bd0294733ae4
Sufian Abu-Rab <s.aburab@gmail.com>
2021-06-29 20:54:32 +0200

    Update createSelfSigned.sh
    increase valid until amount to 5000 (almost 14 years)

--- createSelfSigned.sh
+++ createSelfSigned.sh
@@ -3,9 +3,9 @@
 if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     # Do something under GNU/Linux platform
     openssl req -new -sha256 -nodes -out certs/server.csr -newkey rsa:2048 -keyout certs/server.key -config server.csr.cnf
-    openssl x509 -req -in certs/server.csr -CA certs/rootCA.pem -CAkey certs/rootCA.key -CAcreateserial -out certs/server.crt -days 500 -sha256 -extfile v3.ext
+    openssl x509 -req -in certs/server.csr -CA certs/rootCA.pem -CAkey certs/rootCA.key -CAcreateserial -out certs/server.crt -days 5000 -sha256 -extfile v3.ext
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ] || [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
     # Do something under 32 bits Windows NT platform
     winpty openssl req -new -sha256 -nodes -out certs/server.csr -newkey rsa:2048 -keyout certs/server.key -config server.csr.cnf
-    winpty openssl x509 -req -in certs/server.csr -CA certs/rootCA.pem -CAkey certs/rootCA.key -CAcreateserial -out certs/server.crt -days 500 -sha256 -extfile v3.ext
+    winpty openssl x509 -req -in certs/server.csr -CA certs/rootCA.pem -CAkey certs/rootCA.key -CAcreateserial -out certs/server.crt -days 5000 -sha256 -extfile v3.ext
 fi
